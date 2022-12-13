ab34270b0efbe475e91dc1ad7f87e82c08193b65
晒晕的兔子 <gubu1986@126.com>
2022-12-13 22:47:34 +0800

    fix cancel btn bug

--- Yamler/NewItemView.swift
+++ Yamler/NewItemView.swift
@@ -73,7 +73,7 @@ struct NewItemView: View {
                     } label: {
                         Text("Cancel")
                     }
-                }
+                }.buttonStyle(.plain)
 
             }
          
